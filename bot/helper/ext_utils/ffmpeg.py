02361450dc88501475d08cf166c50a6cbb5a4ef2
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-09-27 02:29:59 +0530

    Update ffmpeg.py

--- bot/helper/ext_utils/ffmpeg.py
+++ bot/helper/ext_utils/ffmpeg.py
@@ -12,31 +12,51 @@ LOGGER = logging.getLogger(__name__)
 
 ########-------- Metadata -------------#########
 async def edit_metadata(listener, base_dir: str, media_file: str, outfile: str, metadata: str = ''):
-    file_name = os.path.basename(media_file)  # Get the filename without path
-    file_namex = re_sub(r'www\S+', '', file_name)
-    title_metadata = f'{metadata}{file_namex}'
+    # Extract clean filename (remove path + unwanted www links)
+    file_name = os.path.basename(media_file)
+    file_namex = re.sub(r'www\S+', '', file_name)
 
+    # Build metadata title
+    title_metadata = f"{metadata} {file_namex}".strip()
+
+    # ffmpeg command
     cmd = [
-        bot_cache['pkgs'][2], '-i', media_file,
-        '-map', '0',
-        '-metadata', f'title={title_metadata}',
-        '-metadata:s:v', f'title={metadata}',
-        '-metadata:s:a', f'title={metadata}',
-        '-metadata:s:s', f'title={metadata}', 
-        '-c', 'copy',
-        outfile
+        bot_cache['pkgs'][2], "-hide_banner", "-ignore_unknown", "-i", media_file,
+        "-map", "0:v:0?", "-map", "0:a:?", "-map", "0:s:?",
+
+        # Metadata
+        "-metadata", f"title={title_metadata}",
+        "-metadata:s:v", f"title={metadata}",
+        "-metadata:s:a", f"title={metadata}",
+        "-metadata:s:s", f"title={metadata}",
+        "-metadata", "Comment=𝗦𝗘𝗔𝗥𝗖𝗛 𝗢𝗡 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 - @𝗔𝗗𝗗𝗔𝗙𝗜𝗟𝗘𝗦",
+        "-metadata", "Copyright=",
+        "-metadata", "AUTHOR=𝗔𝗗𝗔 𝗙𝗜𝗟𝗘𝗦",
+        "-metadata", "Encoded by=𝗔𝗗𝗔 𝗙𝗜𝗟𝗘𝗦",
+        "-metadata", "Encoded_by=@𝗔𝗗𝗗𝗔𝗙𝗜𝗟𝗘𝗦",
+        "-metadata", "Description=",
+        "-metadata", "description=",
+        "-metadata", "SUMMARY=",
+        "-metadata", "WEBSITE=",
+
+        # Copy streams (no re-encode)
+        "-c:v", "copy", "-c:a", "copy", "-c:s", "copy",
+        outfile, "-y"
     ]
-  
+
+    # Run subprocess
     listener.suproc = await create_subprocess_exec(*cmd, stderr=PIPE)
     code = await listener.suproc.wait()
-    
+
     if code == 0:
         await clean_target(media_file)
         listener.seed = False
         await move(outfile, base_dir)
     else:
         await clean_target(outfile)
-        LOGGER.error('%s. Changing metadata failed, Path %s', await listener.suproc.stderr.read().decode(), media_file)
+        error_log = (await listener.suproc.stderr.read()).decode()
+        LOGGER.error("%s. Changing metadata failed, Path %s", error_log, media_file)
+
 
 
 ########-------- Attachment -------------#########
@@ -70,4 +90,5 @@ async def edit_attachment(listener, base_dir: str, media_file: str, outfile: str
         await move(outfile, base_dir)
     else:
         await clean_target(outfile)
+
         LOGGER.error('%s. Changing failed, Path %s', await listener.suproc.stderr.read().decode(), media_file)
